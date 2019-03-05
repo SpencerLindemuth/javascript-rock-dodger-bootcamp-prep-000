5ad8c8373294c0f473b3290e7dd8627911450db5
Spencer Lindemuth <lindemut@gmail.com>
2019-03-05 06:48:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -148,16 +148,16 @@ function moveDodger(e) {
 
   var key = e.which
 
-      if ([LEFT_ARROW, RIGHT_ARROW].indexOf(key) > -1) {
-        e.preventDefault()
-        e.stopPropagation()
+  if ([LEFT_ARROW, RIGHT_ARROW].indexOf(key) > -1) {
+      e.preventDefault()
+      e.stopPropagation()
       }
 
-     if(e.which === LEFT_ARROW){
-       moveDodgerLeft()
+   if(e.which === LEFT_ARROW){
+     moveDodgerLeft()
      }
-     else if(e.which === RIGHT_ARROW){
-       moveDodgerRight()
+   else if(e.which === RIGHT_ARROW){
+     moveDodgerRight()
      }
 
 }
